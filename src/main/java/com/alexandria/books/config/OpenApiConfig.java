f2e95b4304009b2ba2242faaa6afd39f1e44faed
chandra <chandrafirst67@gmail.com>
2022-12-10 15:23:50 +0700

    remove github workflow

--- src/main/java/com/alexandria/books/config/OpenApiConfig.java
+++ src/main/java/com/alexandria/books/config/OpenApiConfig.java
@@ -11,6 +11,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+  name = "bearerAuth",
+  type = SecuritySchemeType.HTTP,
+  bearerFormat = "JWT",
+  scheme = "bearer"
+)
 public class OpenApiConfig {
 
   @Bean
